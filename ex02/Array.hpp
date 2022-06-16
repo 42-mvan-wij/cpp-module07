a420afee2e6740842bdb923320ef383e79dc19d5
Mariusmivw <mvan-wij@student.codam.nl>
2022-06-16 16:40:40 +0200

    add ex02

---
+++ ex02/Array.hpp
@@ -0,0 +1,44 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <stdexcept>
+
+template<typename T>
+class Array {
+	public:
+		Array<T>() : content(new T[0]), n(0) {};
+		Array<T>(unsigned int n) : content(new T[n]), n(n) {};
+
+		~Array<T>() { delete[] content; };
+
+		Array<T>(Array<T>& src) : n(src.size()) {
+			content = new T[n];
+			for (unsigned int i = 0; i < n; i++) {
+				content[i] = src[i];
+			}
+		};
+
+		Array<T>& operator=(Array<T>& rhs) {
+			delete[] content;
+			n = rhs.size();
+			content = new T[n];
+			for (unsigned int i = 0; i < n; i++) {
+				content[i] = rhs[i];
+			}
+			return *this;
+		};
+
+		T& operator[](unsigned int i) {
+			if (i >= n)
+				throw std::exception();
+			return content[i];
+		};
+
+		unsigned int size() const { return n; };
+
+	protected:
+		T* content;
+		unsigned int n;
+};
+
+#endif
